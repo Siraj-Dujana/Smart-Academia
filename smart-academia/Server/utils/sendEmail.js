edae17774855113639f0bb76e756f763780b1a1e
Siraj-Dujana <dujanadujana16@gmail.com>
2026-08-07 04:53:03 +0500

    deploy: it's ready to deploy on vercel

--- smart-academia/Server/utils/sendEmail.js
+++ smart-academia/Server/utils/sendEmail.js
@@ -4,13 +4,13 @@ const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: process.env.GMAIL_USER,
-      pass: process.env.GMAIL_PASS,
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
     },
   });
 
   const mailOptions = {
-    from: `"SmartAcademia" <${process.env.GMAIL_USER}>`,
+    from: `"SmartAcademia" <${process.env.EMAIL_USER}>`,
     to,
     subject,
     html,
